6247032d77ee83bd5664945dad74b930503b9979
mateoleont <mateolt505@gmail.com>
2023-09-25 01:27:11 -0500

    inicio Front

---
+++ client/src/views/Landing/landing.jsx
@@ -0,0 +1,29 @@
+// Este componente es el Landing Page
+//----------------------------------------------
+import './landing.css';
+import { useHistory } from 'react-router-dom';
+//----------------------------------------------
+const Landing = () =>
+{
+    const history = useHistory();
+
+    const handleButtonClick = () => 
+    {
+        history.push('/home'); // Redirige al enlace '/home' al hacer clic en el botón
+    };
+
+    return(
+        <div >
+            <div className = "landing-images">
+                <img src = '' alt = "Pokemon APP"/>
+            </div>
+            <span>Holi</span>
+            <br />
+            <button className = 'landingButton' onClick = {handleButtonClick}>
+                Vamos!
+            </button>
+        </div>
+    );
+}
+//----------------------------------------------
+export default Landing;
---
+++ client/src/views/index.js
@@ -0,0 +1,5 @@
+// Este módulo es para importar directamente las views 
+//----------------------------------------------
+import Landing from './Landing/landing.jsx';
+//----------------------------------------------
+export { Landing, };
