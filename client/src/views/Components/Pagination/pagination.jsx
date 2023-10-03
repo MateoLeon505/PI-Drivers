5e9c2c20b401a6e302cb93297a951ac21b0d2df4
mateoleont <mateolt505@gmail.com>
2023-10-02 20:19:54 -0500

    pagination

--- client/src/views/Components/Pagination/pagination.jsx
+++ client/src/views/Components/Pagination/pagination.jsx
@@ -1,5 +1,21 @@
 const Pagination = ({ pagination, totalOfPages }) =>
 {
     const pages = [];
+    //---------------
+    for (let i = 0; i <= totalOfPages; i++) pages.push(i);
+    //---------------
+    return(
+        <div className = "pagination">
+            {
+                pages.map((page) => 
+                (
+                    <button>
+                        {page}
+                    </button>
+                ))
+            }
+        </div>
+    );
 }
 //---------------------------------------------- 
+export default Pagination;
--- client/src/views/Home/home.jsx
+++ client/src/views/Home/home.jsx
@@ -4,27 +4,29 @@ import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getDrivers, getDriverById } from '../../redux/actions.js';
 import Drivers from '../Components/Drivers/drivers.jsx';
+import Pagination from '../Components/Pagination/pagination.jsx';
 import gifLoading from '../../Images/gifLoading.gif';
 import './home.css';
 //----------------------------------------------
 const Home = () =>
 {
-    const dispatch = useDispatch();
-
-    const [ currentPage, setCurrentPage ] = useState(1); // Núm para páginas
-
+    const dispatch = useDispatch(); // para despachar acciones
+    //---------------
+    const [ currentPage, setCurrentPage ] = useState(1); // Núm de página
+    //---------------
+    // Trae Drivers cuando se monta el componente
     useEffect(() =>
     {
         dispatch(getDrivers());
     },[dispatch]);
-
+    //---------------
     const allDrivers = useSelector(state => state.drivers); // Lista de TODOS los Drivers
-    console.log(allDrivers);
     //------------------------------------------
     return(
         <div className = 'homeContainer'>
             <h1 className = 'titles'>DRIVERS</h1>
             <Drivers collectionOfDrivers = {allDrivers}/>
+            <Pagination></Pagination>
             <img src = {gifLoading} alt="" />
         </div>
     );
