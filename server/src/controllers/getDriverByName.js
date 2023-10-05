484f7c14a38af364b91a2ff2bdc381603eebffbc
mateoleont <mateolt505@gmail.com>
2023-10-04 20:24:10 -0500

    getByName

--- server/src/controllers/getDriverByName.js
+++ server/src/controllers/getDriverByName.js
@@ -10,65 +10,44 @@ const getDriverByName = async (name) =>
     // 1ra letra mayúsc. y el resto minúsculas
     name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase(); // Insensible a may o min
     // Trae Divers de la bd
-    let dbDriverByName = await Driver.findOne(
+    let dbDriverByName = await Driver.findAll(
     {
         where: { 
-            forename: { [Op.iLike]: `%${name}%`, // Búsqueda insensible a May. o Min.
-            },
+            [Op.or]: [
+                {forename: { [Op.iLike]: `%${name}%` }}, // Búsqueda insensible a May. o Min.
+                {surname: { [Op.iLike]: `%${name}%` }},
+            ],
+            
         },
         limit: 15, // Limite 15 resultados
     });
-    if (!dbDriverByName) 
-    {
-        dbDriverByName = await Driver.findOne(
-            {
-                where: { 
-                    surname: { [Op.iLike]: `%${name}%`, // Búsqueda insensible a May. o Min.
-                    },
-                },
-                limit: 15, // Limite 15 resultados
-            });
-    }
     //-------------------------------
-    if (dbDriverByName) // Si se encontró en la bd
+    if (dbDriverByName.length >= 15) // Si se encontró en la bd
     {
-    // TRAE TEAMS:
-    const id = dbDriverByName.id;
-    // Busca driver por 'Primary Key' e incluye a los teams
-    const dbDriverById = await Driver.findByPk(id, {include: [{ model: Team, as: 'teams' }]});
-    let teams = dbDriverById.teams.map((team) => team.name); // Trae únicamente nombre de los 'teams'
-    teams = teams.toString();
-    //------------------------------- 
-        const driver = 
-        {
-            id: dbDriverByName.id,
-            forename: dbDriverByName.forename,
-            surname: dbDriverByName.surname,
-            description: dbDriverByName.description,
-            image: dbDriverByName.image,
-            nationality: dbDriverByName.nationality,
-            dob: dbDriverByName.dob,
-            teams: teams
-        };
-        return driver;  
+        return dbDriverByName;  
     }
-    else // Si no, busca en la Api
+
+    if (dbDriverByName.length < 15) 
     {
         let apiDriverByName = (await axios.get(`http://localhost:5000/drivers?name.forename=${name}`)).data;
         if (apiDriverByName.length === 0) apiDriverByName = (await axios.get(`http://localhost:5000/drivers?name.surname=${name}`)).data;
+
+        // Completar hasta 15 resultados si hay datos en la API
+        const remainingResults = 15 - dbDriverByName.length;
         if (apiDriverByName.length > 0) 
         {   
-            return apiDriverByName.slice(0, 15).map((driver) =>
+            const apiResults = apiDriverByName.slice(0, remainingResults).map((driver) =>
             ({
-                id: apiDriverByName[0].id,
-                forename: apiDriverByName[0].name.forename,
-                surname: apiDriverByName[0].name.surname,
-                description: apiDriverByName[0].description,
-                image: apiDriverByName[0].image.url,
-                nationality: apiDriverByName[0].nationality,
-                dob: apiDriverByName[0].dob,
-                teams: apiDriverByName[0].teams
+                id: driver.id,
+                forename: driver.name.forename,
+                surname: driver.name.surname,
+                description: apiDriverByName.description,
+                image: driver.image.url,
+                nationality: driver.nationality,
+                dob: driver.dob,
+                teams: driver.teams
             })); 
+            return [...dbDriverByName, ...apiResults];
         }
         else
         {
