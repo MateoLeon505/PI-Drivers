b75be5b635b90cc7690c6f2ef4bd0f8434b128e4
mateoleont <mateolt505@gmail.com>
2023-10-10 17:02:03 -0500

    getByName

--- server/src/controllers/getDriverByName.js
+++ server/src/controllers/getDriverByName.js
@@ -1,15 +1,15 @@
 // Este módulo tiene la responsabilidad de Traer a un Driver por nombre
 //-------------------------------------
-// Importación de módulos
-const { Driver, Team } = require('../db'); // Trae los modelos
-const axios = require("axios"); // Para solicitudes HTTP
+const { Driver, Team } = require('../db'); 
+const axios = require("axios"); 
 const { Op } = require('sequelize');
 //-------------------------------------
 const getDriverByName = async (name) =>
 {
-    // 1ra letra mayúsc. y el resto minúsculas
     name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase(); // Insensible a may o min
-    // Trae Divers de la bd
+    let combinedResults = [];
+    //----------------------
+    // BUSCA Divers en la bd
     let dbDriverByName = await Driver.findAll(
     {
         where: { 
@@ -17,44 +17,54 @@ const getDriverByName = async (name) =>
                 {forename: { [Op.iLike]: `%${name}%` }}, // Búsqueda insensible a May. o Min.
                 {surname: { [Op.iLike]: `%${name}%` }},
             ],
-            
         },
-        limit: 15, // Limite 15 resultados
     });
+    if (dbDriverByName.length > 0) combinedResults.push(...dbDriverByName);
     //-------------------------------
-    if (dbDriverByName.length >= 15) // Si se encontró en la bd
-    {
-        return dbDriverByName;  
+    // BUSCA Divers en la API
+    let apiDriverByForeName = (await axios.get(`http://localhost:5000/drivers?name.forename=${name}`)).data;
+    let apiDriverBysurName = (await axios.get(`http://localhost:5000/drivers?name.surname=${name}`)).data;
+
+    if (apiDriverByForeName.length > 0) 
+    {   
+        apiDriverByForeName = apiDriverByForeName.map((driver) =>
+        ({
+            id: driver.id,
+            forename: driver.name.forename,
+            surname: driver.name.surname,
+            description: driver.description,
+            image: driver.image.url,
+            nationality: driver.nationality,
+            dob: driver.dob,
+            teams: driver.teams
+        })); 
+        combinedResults.push(...apiDriverByForeName);
     }
-
-    if (dbDriverByName.length < 15) 
+    if (apiDriverBysurName.length > 0) 
+    {   
+        apiDriverBysurName = apiDriverBysurName.map((driver) =>
+        ({
+            id: driver.id,
+            forename: driver.name.forename,
+            surname: driver.name.surname,
+            description: driver.description,
+            image: driver.image.url,
+            nationality: driver.nationality,
+            dob: driver.dob,
+            teams: driver.teams
+        })); 
+        combinedResults.push(...apiDriverBysurName);
+    }
+    //----------------------------------------------------------------
+    if (combinedResults.length > 0) 
     {
-        let apiDriverByName = (await axios.get(`http://localhost:5000/drivers?name.forename=${name}`)).data;
-        if (apiDriverByName.length === 0) apiDriverByName = (await axios.get(`http://localhost:5000/drivers?name.surname=${name}`)).data;
-
-        // Completar hasta 15 resultados si hay datos en la API
-        const remainingResults = 15 - dbDriverByName.length;
-        if (apiDriverByName.length > 0) 
-        {   
-            const apiResults = apiDriverByName.slice(0, remainingResults).map((driver) =>
-            ({
-                id: driver.id,
-                forename: driver.name.forename,
-                surname: driver.name.surname,
-                description: apiDriverByName.description,
-                image: driver.image.url,
-                nationality: driver.nationality,
-                dob: driver.dob,
-                teams: driver.teams
-            })); 
-            return [...dbDriverByName, ...apiResults];
-        }
-        else
-        {
-            return {message: 'No existe ningún driver con ese nombre'};
-        }
-    }  
+        if (combinedResults.length > 15) return combinedResults.slice(0, 15);    
+        else return combinedResults;    
+    }
+    else
+    {
+        return {message: 'No existe ningún driver con ese nombre'};
+    }
 }
 //-------------------------------------
-// Exporta la función
 module.exports = { getDriverByName };
